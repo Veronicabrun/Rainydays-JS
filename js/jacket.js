c912032b4ec1146b91cd6f32a6ebcea106ca5fa0
Veronicabrun <105516840+Veronicabrun@users.noreply.github.com>
2023-10-18 20:16:15 +0200

    js

---
+++ js/jacket.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+//const productContainer = document.querySelector(".jackets-details");
+
+//const queryString = document.location.search;
+
+//const params = new URLSearchParams(queryString);
+
+//const id = params.get("id");
+
+//console.log(id);
+
+
+//const url = "https://api.noroff.dev/api/v1/rainy-days/" + id;
+
+//console.log(url);
+
+//async function getProduct() {
+    
+    //try {
+        //const response = await fetch(url);
+        //const details = await response.json();
+
+        //console.log(details);
+    
+        //createHtml(details);
+
+    //}
+     //catch(error) {
+        //console.log(error)
+        //productContainer.innerHTML = displayError("An error occured", error);
+    //}
+ 
+//}
+
+//getProduct();
+
+//function createHtml(details) {
+    //productContainer.innerHTML = `<h1>${details.name}</h1>
+                                 //<div class="details-image"
+                                     //style=background-image: url(${details.background_image})"></div>
+                                 //<div class="details-description">${details.description}</div>`;
+//}
---
+++ js/jackets.js
@@ -0,0 +1,100 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const productListContainer = document.getElementById('jackets-details');
+    const apiUrl = 'https://api.noroff.dev/api/v1/rainy-days';
+
+    async function fetchProducts() {
+        try {
+            const response = await fetch(apiUrl);
+            const products = await response.json();
+            return products;
+        } catch (error) {
+            console.error('Error fetching products:', error);
+            return null;
+        }
+    }
+
+    async function displayProducts() {
+        const products = await fetchProducts();
+
+        if (products) {
+            products.forEach(product => {
+                const productElement = document.createElement('div');
+                productElement.classList.add('product');
+                productElement.innerHTML = `
+                    <h3>${product.name}</h3>
+                    <button class="viewDetails" data-id="${product.id}">View Details</button>
+                `;
+                productListContainer.appendChild(productElement);
+            });
+
+            const viewDetailsButtons = document.querySelectorAll('.viewDetails');
+            viewDetailsButtons.forEach(button => {
+                button.addEventListener('click', viewProductDetails);
+            });
+        } else {
+            productListContainer.innerHTML = 'Error fetching products. Please try again later.';
+        }
+    }
+
+    async function viewProductDetails(event) {
+        const productId = event.target.dataset.id;
+
+        try {
+            const response = await fetch(`${apiUrl}/${productId}`);
+            const productDetails = await response.json();
+
+            console.log('Product Details:', productDetails);
+        } catch (error) {
+            console.error('Error fetching product details:', error);
+        }
+    }
+
+    displayProducts();
+});
+
+
+
+
+
+
+
+
+
+
+//const productContainer = document.querySelector(".jackets-details");
+
+//const url = "https://api.noroff.dev/api/v1/rainy-days";
+
+
+//async function getProduct() {
+    
+    //try {
+        //const response = await fetch(url);
+        //const json = await response.json();
+
+       // console.log(json);
+    
+        //productContainer.innerHTML = "";
+
+        //const jackets = json.results;
+
+        //jackets.forEach(function(jacket) {
+            //productContainer.innerHTML += `<a href=details.html?id=${jackets.id} class="card">
+                                 //<div class="image"style=background-image: url(${jackets.background_image})"></div>
+                                 //<div class="details">
+                                     //<h4 class="name"> ${jackets.name}</h4>
+                                 //</div>`;
+
+
+        //});
+        
+            
+    //}    
+    //catch(error) {
+        //console.log(error)
+        //productContainer.innerHTML = message("error", error);
+    //}
+
+//}
+
+//getProduct();
