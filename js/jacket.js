7e364f8c6028f233bdb861727a5e00d910d5f813
Veronicabrun <105516840+Veronicabrun@users.noreply.github.com>
2023-10-26 23:30:09 +0200

    .

--- js/jacket.js
+++ js/jacket.js
@@ -1,52 +1,90 @@
+//document.addEventListener('DOMContentLoaded', () => {
+  //  const urlParams = new URLSearchParams(window.location.search);
+    //const productId = urlParams.get('id');
 
+    //const apiUrl = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
 
+    //async function getProductDetails() {
+        //try {
+            //const response = await fetch(apiUrl);
+            //const result = await response.json();
 
+            //const loadingIndicator = document.querySelector('.loading-indicator');
+            //const productDetails = document.querySelector('.product-card product-featured');
 
+            //for (let i = 0; i < 5; i++) {
+                //setTimeout(() => {
+                    //loadingIndicator.style.display = 'none';
+                    //productDetails.style.display = 'block';
 
+                    //const productImage = document.getElementById('product-image');
+                    //const productTitle = document.getElementById('product-title');
+                    //const productPrice = document.getElementById('product-price');
 
-
-
-
-
-
-//const productContainer = document.querySelector(".jackets-details");
-
-//const queryString = document.location.search;
-
-//const params = new URLSearchParams(queryString);
-
-//const id = params.get("id");
-
-//console.log(id);
-
-
-//const url = "https://api.noroff.dev/api/v1/rainy-days/" + id;
-
-//console.log(url);
-
-//async function getProduct() {
-    
-    //try {
-        //const response = await fetch(url);
-        //const details = await response.json();
-
-        //console.log(details);
-    
-        //createHtml(details);
-
+                    //productImage.src = result.image;
+                    //productImage.alt = result.title;
+                    //productTitle.textContent = result.title;
+                    //productPrice.textContent = result.price;
+                //}, 1000 * i);
+            //}
+        //} catch (error) {
+          //  console.log(error);
+        //}
     //}
-     //catch(error) {
-        //console.log(error)
-        //productContainer.innerHTML = displayError("An error occured", error);
-    //}
- 
-//}
 
-//getProduct();
+    //getProductDetails();
+//});
 
-//function createHtml(details) {
-    //productContainer.innerHTML = `<h1>${details.name}</h1>
-                                 //<div class="details-image"
-                                     //style=background-image: url(${details.background_image})"></div>
-                                 //<div class="details-description">${details.description}</div>`;
-//}
+
+document.addEventListener('DOMContentLoaded', async function() {
+    // This function waits for the DOM content to fully load before executing any JavaScript code.
+
+    const productListContainer = document.getElementById('product-image image-specification');
+    const loadingIndicator = document.getElementById('loading-indicator');
+
+    // `productListContainer` is a reference to an HTML element with the ID 'product-list'. This is where we will display the list of products.
+    // `loadingIndicator` is a reference to an HTML element with the ID 'loading-indicator'. This element will be used to show a loading message while we fetch data from the API.
+
+    // Function to fetch products from API
+    async function fetchProducts() {
+        // This is an asynchronous function that will fetch the products from the API.
+
+        try {
+            const response = await fetch('https://api.noroff.dev/api/v1+id');
+            // Here, we use the `fetch` function to make a GET request to the API's endpoint that provides a list of products.
+            // `await` is used to wait for the response to come back before proceeding.
+
+            const data = await response.json();
+            // Once we get the response, we use `await` again to parse it as JSON. This gives us an array of product objects.
+
+            loadingIndicator.style.display = 'none';
+            // After we receive the data, we hide the loading indicator.
+
+            for (let i = 0; i < data.length; i++) {
+                // We use a `for` loop to iterate over each product in the `data` array.
+                // `let i = 0` initializes a loop variable `i` to 0.
+                // `i < data.length` defines the condition for the loop to continue.
+                // `i++` increments `i` after each iteration.
+
+                const product = data[i];
+                // `product` now holds the current product object in the loop.
+
+                const productItem = document.createElement('div');
+                // We create a new HTML `div` element to represent each product in the list.
+
+                productItem.innerHTML = `<a href="jacket.html?id=${product.id}">${product.name}</a>`;
+                // We set the inner HTML of the `div` element. It contains an anchor (`<a>`) element that links to the product details page.
+                // The `href` attribute includes a query string parameter `id` with the value of the product's ID.
+
+                productListContainer.appendChild(productItem);
+                // We append the newly created `div` element (representing the product) to the `productListContainer`.
+            }
+        } catch (error) {
+            loadingIndicator.innerHTML = 'Error fetching data';
+            // If an error occurs during the fetching process, we catch it and display an error message in the loading indicator element.
+        }
+    }
+
+    await fetchProducts();
+    // Finally, we call the `fetchProducts` function. We use `await` to wait for the function to complete before moving on.
+});
--- js/jackets.js
+++ js/jackets.js
@@ -1,154 +1,38 @@
-//document.addEventListener('DOMContentLoaded', () => {
-    const productListContainer = document.getElementById('jackets-details');
+document.addEventListener('DOMContentLoaded',() => {
+    const productContainer = document.querySelector('.products-container');
+    //const loadingIndicator = document.getElementById('loading-indicator');
     const apiUrl = 'https://api.noroff.dev/api/v1/rainy-days';
 
-    async function fetchProducts() {
-        try {
-            const response = await fetch(apiUrl);
-            const products = await response.json();
-            return products;
-        } catch (error) {
-            console.error('Error fetching products:', error);
-            return null;
-        }
-    }
-
-    async function displayProducts() {
-        const products = await fetchProducts();
-
-        if (products) {
-            products.forEach(product => {
-                const productElement = document.createElement('div');
-                productElement.classList.add('product');
-                productElement.innerHTML = `
-                    <h3>${product.name}</h3>
-                    <button class="viewDetails" data-id="${product.id}">View Details</button>
-                `;
-                productListContainer.appendChild(productElement);
-            });
-
-            const viewDetailsButtons = document.querySelectorAll('.viewDetails');
-            viewDetailsButtons.forEach(button => {
-                button.addEventListener('click', viewProductDetails);
-            });
-        } else {
-            productListContainer.innerHTML = 'Error fetching products. Please try again later.';
-        }
-    }
-
-    async function viewProductDetails(event) {
-        const productId = event.target.dataset.id;
-
-        try {
-            const response = await fetch(`${apiUrl}/${productId}`);
-            const productDetails = await response.json();
-
-            console.log('Product Details:', productDetails);
-        } catch (error) {
-            console.error('Error fetching product details:', error);
-        }
-    }
-
-    displayProducts();
-//});
-
-
-
-
-
-
-
-
-
-//const productContainer = document.querySelector(".all-jackets");
-
-//const url = "https://api.noroff.dev/api/v1/rainy-days";
-
-
-//async function getProduct() {
-    
-   // try {
-        //const response = await fetch(url);
-        //const results = await response.json();
-
-    
-        //productContainer.innerHTML = "";
-
-        //const jackets = json.results;
-
-        //jackets.forEach(function(jacket) {
-           // productContainer.innerHTML += `<a href=details.html?id=${jackets.id} class="card">
-                                // <div class="image"style=background-image: url(${jackets.background_image})"></div>
-                                // <div class="details">
-                                    // <h4 class="name"> ${jackets.name}</h4>
-                                 //</div>`;
-
-
-        //});
-        
-            
-    //}    
-   // catch(error) {
-        //console.log(error)
-        //productContainer.innerHTML = message("error", error);
-    //}
-
-//}
-
-//getProduct();
-//let keyword because the value of html will change inside the loop
-//let html = "";
-
-//for (let i = 0; i < jacket.length; i++) {
-     //html = html + "<h1>" + jackets[i].type + "</h1>";
-
-    // html = html + `<h1>${jackets[i].type}</h1>`;
-
-    //div class jackets, er div'en du kan style i css
-     //html += `<div class="jackets">
-                //<h1>${jackets[i].type}</h1>
-                //<p>info: ${jakets[i].info}</p>
-
-              //</div>
-              //`;
-
-    //console.log(html);
-
-
-//CSSContainerRule.innerHTML = html;
-
-
-const url = "https://api.noroff.dev/api/v1/rainy-days";
-
-const productContainer = document.querySelector(".show-jackets");
-
 async function getProduct() {
-    
-     
-         const response = await fetch(url);
+    try {
+        const response = await fetch(apiUrl);
+        const results = await response.json();
 
-         const results = await response.json();
-  
-         console.log(results);
+        console.log(results);
 
+        productContainer.innerHTML = '';
 
-        productContainer.innerHTML = "";
+        //loadingIndicator.style.display = 'none';
 
-         for (let i = 0; i < product.length; i++) {
-            console.log(product[i].text);
+        for (let i = 0; i < results.length; i++) {
+            if (i === 5) {
+                break;
+            }
 
-             if (i === 10) {
-               break;
-             }
-
-             productContainer.innerHTML += `<div class="showjackets">${product[i].text}</div>`;
-         }
-
-}   
-
- catch (error) {
-    console.log(error);
-    productContainer.innerHTML = alert("error", error);
+            productContainer.innerHTML += `<div class="product-card product-featured">
+            <a href="jacket.html?id=${results[i].id}">
+            <img class="product__image" src="${results[i].image}" alt="${results[i].title}">
+            </a>
+            <h2>${results[i].title}</h2>
+            <p class="product__price">${results[i].price}</p>
+            </div>`;
+        }
+    } catch (error) {
+        console.log(error);
+        productContainer.innerHTML = message('error', error);
+    }
 }
 
 getProduct();
+
+});
